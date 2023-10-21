c02bc8a97886834e56d015fbe74358cfcf1eedbc
eunung <eunung@gmail.com>
2023-10-21 20:13:45 +0900

    feat: 배틀존 영역 처리

--- index.js
+++ index.js
@@ -10,16 +10,35 @@ for (let i = 0; i < collisions.length; i += 70) {
     collisionsMap.push(collisions.slice(i, i + 70));
 }
 
-const boundaries = [];
+const battleZonesMap = [];
+for (let i = 0; i < battleZonesData.length; i += 70) {
+    battleZonesMap.push(battleZonesData.slice(i, i + 70));
+}
 
 const offset = {
     x: -735,
     y: -630
 }
+
+const boundaries = [];
 collisionsMap.forEach((row, i) => {
     row.forEach((symbol, j) => {
         if (symbol === 1025) {
             boundaries.push(new Boundary({
+                position: {
+                    x: j * Boundary.width + offset.x,
+                    y: i * Boundary.height + offset.y,
+                }
+            }));
+        }
+    })
+});
+
+const battleZones = [];
+battleZonesMap.forEach((row, i) => {
+    row.forEach((symbol, j) => {
+        if (symbol === 1025) {
+            battleZones.push(new Boundary({
                 position: {
                     x: j * Boundary.width + offset.x,
                     y: i * Boundary.height + offset.y,
@@ -97,7 +116,7 @@ const keys = {
     }
 }
 
-const movables = [background, ...boundaries, foreground];
+const movables = [background, ...boundaries, foreground, ...battleZones];
 
 function rectangularCollision({ rectangle1, rectangle2 }) {
     return (
@@ -111,11 +130,36 @@ function animate() {
     window.requestAnimationFrame(animate);
     background.draw();
     boundaries.forEach(boundary => boundary.draw());
+    battleZones.forEach(battleZone => battleZone.draw());
     player.draw();
     foreground.draw();
 
     let moving = true;
     player.moving = false;
+
+    if (keys.w.pressed || keys.a.pressed || keys.s.pressed || keys.d.pressed) {
+        for (let i = 0; i < battleZones.length; i++) {
+            const battleZone = battleZones[i];
+            const overlappingArea = (
+                    Math.min(player.position.x + player.width, battleZone.position.x + battleZone.width)
+                    - Math.max(player.position.x, battleZone.position.x)
+                ) * (
+                    Math.min(player.position.y + player.height, battleZone.position.y + battleZone.height)
+                    -Math.max(player.position.y, battleZone.position.y)
+                );
+            if (rectangularCollision({
+                rectangle1: player,
+                rectangle2: battleZone
+            })
+                && overlappingArea > (player.width * player.height) / 2
+                && Math.random() < 0.01 //  배틀존에서 다닐 때 랜덤으로 배틀하기 위함
+            ) {
+                console.log('battle zone');
+                break;
+            }
+        }
+    }
+
     if (keys.w.pressed && lastKey === 'w') {
         player.moving = true;
         player.image = player.sprites.up;
@@ -142,6 +186,7 @@ function animate() {
                 break;
             }
         }
+
         if (moving) {
             movables.forEach((movable) => movable.position.y += 3);
         }
